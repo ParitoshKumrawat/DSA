769adb895e85ac15294fce4533fc82b2e2f52705
Paritosh Kumrawat <166283576+ParitoshKumrawat@users.noreply.github.com>
2026-08-24 16:00:30 +0530

    Time: 233 ms (55.08%) | Memory: 42.8 MB (74.98%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board){
+        for(int row = 0 ; row < 9 ; row++){
+          for(int col = 0 ; col < 9 ; col++){
+
+            if(board[row][col] == '.'){
+
+                for(char num = '1' ; num <= '9' ; num++){
+
+                    if(validPos(board, row, col, num)){
+                        board[row][col] = num;
+
+                        if(solve(board)){
+                            return true;
+                        }
+
+                        board[row][col] = '.';
+                    }
+                }
+
+                return false;
+            }
+          }
+        }
+
+        return true;
+    }
+
+    private boolean validPos(char[][] board, int row, int col, char num){
+        for(int i = 0 ; i < 9 ; i++){
+           if(board[row][i] == num)return false;
+        }
+
+        for(int i = 0 ; i < 9 ; i++){
+            if(board[i][col] == num)return false;
+        }
+
+        int rowStart = (row / 3) * 3 ;
+        int colStart = (col / 3) * 3; 
+
+        for(int i = rowStart ; i < rowStart + 3 ; i++){
+         for(int j = colStart ; j < colStart + 3 ; j++){
+            if(board[i][j] == num)return false;
+         }
+        }
+
+        return true;
+    }
+}
